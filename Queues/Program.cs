8750fb23e264586630fd0e7c0a73f7be68a97f15
BhavanaReddy99 <jorrigalabhavana01@gmail.com>
2023-10-31 11:45:17 +0530

    Add project files.

---
+++ Queues/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            runApp();
+        }
+            static void runApp()
+            {
+                Queue queue = new Queue(5);
+
+                // Enqueue elements into the queue
+                queue.Enqueue(10);
+                queue.Enqueue(20);
+                queue.Enqueue(30);
+                queue.Enqueue(40);
+                queue.Enqueue(50);
+                queue.Enqueue(60);
+
+            // Dequeue elements and display them
+            Console.WriteLine("Queue Elements:");
+                while (!queue.IsEmpty())
+                {
+                    int element = queue.Dequeue();
+                    Console.WriteLine(element);
+                }
+            }
+
+        }
+    }
+
---
+++ Queues/Queue.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+   
+
+    public class Queue
+    {
+        private int[] elements;
+        private int front;
+        private int rear;
+        private int max;
+
+        public Queue(int size)
+        {
+            max = size;
+            elements = new int[size];
+            front = 0;
+            rear = -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return front > rear;
+
+        }
+
+        public bool IsFull()
+        {
+            return rear == max - 1;
+        }
+
+        public void Enqueue(int item)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Queue is full. Cannot enqueue.");
+            }
+            else
+            {
+                elements[++rear] = item;
+            }
+        }
+
+        public int Dequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue is empty. Cannot dequeue.");
+                return -1; // You can choose any default value
+            }
+            else
+            {
+                int item = elements[front++];
+                return item;
+            }
+        }
+
+        public string PrintQueue()
+        {
+            string result = "";
+
+            for (int i = front; i <= rear; i++)
+            {
+                result += elements[i] + " ";
+            }
+
+            return result;
+        }
+    }
+
+}
